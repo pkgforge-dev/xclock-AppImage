0e603a9267286f692534ddef563fd7034bdd1018
Link4Electronics <link4electronics@gmail.com>
2026-08-30 07:05:53 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export USE_HOST_DRIVERS_EXPERIMENTAL=1
+export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/xclock /usr/share/X11/app-defaults /usr/lib/libXaw*.so*
